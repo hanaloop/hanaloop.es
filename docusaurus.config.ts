6b9061cdac5aec4270921413ba04dce767699d7d
Young Suk Ahn Park <ys.ahnpark@hanaloop.com>
2023-12-19 16:29:28 +0900

    Add hanaloop social media card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'images/hanaloop-social-card_ko.jpg',
     navbar: {
       title: 'HanaLoop',
       logo: {
