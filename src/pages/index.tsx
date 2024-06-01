772bc0614424d9b703f3d8679641cad22c5b3fd0
Young Suk Ahn Park <ys.ahnpark@hanaloop.com>
2024-06-01 19:13:18 +0900

    Add LRQA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,9 +44,20 @@ export default function Home(): JSX.Element {
         }
       />
       <main>
+        <SectionBlock title={data.content.section0_title} containerStyle='bg-white'>
+          <div className="flex px-10 my-4 text-left">
+            <div className='flex-none w-64' ><img src="/images/partners/lrqa-logo.png" /></div>
+            <div className='w-full space-y-2'>
+              <h2 className='text-3xl font-bold'>로이드인증원(LRQA) 배출권거래제 및 EU 탄소국경조정제도 플랫폼 적합성 국내 첫 검증의견서 획득</h2>
+              <div className='text-lg' >(2024.05.30) 탄소관리 선도기업 ㈜하나루프는 자사의 기후규제 대응 탄소중립 플랫폼인 하나에코가 지난달 30일 국제공인 인증기관인 로이드인증원(LRQA)으로부터 
+                한국 배출권거래제 및 유럽연합(EU)의 탄소국경조정제도(CBAM) 지침에 부합한 것으로 적합성을 인정받았다고 밝혔다. 이는 국내 최초로 하나에코가 국내외적으로 신뢰할 수 있는 탄소관리 솔루션임을 공식 인정받은 것이다.</div>
+            </div>
+          </div>
+        </SectionBlock>
+
         {/* Climate strategy is essential */}
         <SectionBlock title={data.content.section1_title} containerStyle='bg-gray-100'>
-          <div className="px-10  my-5 grid lg:grid-cols-4 grid-cols-1 gap-4  ">
+          <div className="px-10 my-4 grid lg:grid-cols-4 grid-cols-1 gap-4  ">
             {
               data.reasons.map(item => 
                 <div className="border border-green-600 rounded-lg text-center text-gray-700" key={item.title}>
@@ -62,6 +73,7 @@ export default function Home(): JSX.Element {
         <SectionBlock title={data.content.section2_title} containerStyle="" >
           <div className="space-y-10">
             <div className="mt-4 text-center items-center space-y-8 ">
+              {/* CUSTOMERS */}
               <div className="flex justify-center space-x-6 ">
                 {
                   data.customers.map(item => 
@@ -71,6 +83,7 @@ export default function Home(): JSX.Element {
                     </span>)
                 }
               </div>
+              {/* PARTNERS */}
               <div className="flex justify-center space-x-6 ">
                 {
                   data.partners.map(item => 
--- static/plugins/chat-embedded.js
+++ static/plugins/chat-embedded.js
@@ -5,29 +5,29 @@ function init() {
   style.type = "text/css";
   style.innerHTML = `
     .iframe-container {
-    max-height: 800px;
-    right: 6px;
-    bottom: 12px;
-    position: fixed;
-    width: 60px;
-    height: 69px;
+      max-height: 800px;
+      right: 6px;
+      bottom: 12px;
+      position: fixed;
+      width: 85px;
+      height: 85px;
     }
     
     .iframe-container.is-open {
-    bottom: 12px;
-    width: calc(35vw);
-    min-width: 300px;
-    max-width: 500px;
-    left: initial;
-    height: calc(90vh);
-    transition: height 0.2s ease-in-out;
-    z-index: 1000
+      bottom: 12px;
+      left: initial;
+      width: 450px;
+      min-width: 450px;
+      max-width: 500px;
+      height: calc(95vh);
+      transition: height 0.2s ease-in-out;
+      z-index: 1000
     }
 
-    iframe{
-        width: 100%;
-        height: 100%;
-        border: none;
+    iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
     }
     `;
 
@@ -37,6 +37,7 @@ function init() {
   iframeContainer.id = "iframe-container";
   const iframe = document.createElement("iframe");
   iframe.src = "https://hanademo.eco/knoa/chat_plugin";
+  // iframe.src = "http://localhost:3000/knoa/chat_plugin";
   iframeContainer.appendChild(iframe);
   iframe.frameBorder = 0;
   iframe.allow = "clipboard-read; clipboard-write";
