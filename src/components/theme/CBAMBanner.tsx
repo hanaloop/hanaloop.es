80586d60736fe522d1de9241a20e6e19a4804dea
jeongdopark <jeongdo.dev@gmail.com>
2025-05-06 23:52:46 +0900

    fix not found

--- src/components/theme/CBAMBanner.tsx
+++ src/components/theme/CBAMBanner.tsx
@@ -4,7 +4,7 @@ const CBAMBanner = () => {
         <div className="w-full p-5 mt-10 rounded-lg text-center text-base border ">
             <h2 className="text-2xl font-bold text-blue-500 my-4">하나루프, 중소벤처기업부 CBAM 인프라구축 사업 참여기업을 모집합니다</h2>
             CBAM 대응 컨설팅 + 검증과 함께 하나에코 CBAM 솔루션을 정부 지원으로! <br/>
-            <a href="/blog/2025/20250405-cbam-program#지원조건-및-자격">하나루프 X (주)로엔컨설팅과 함께하는 정부지원사업 상세내용 링크</a><br/>
+            <a href="/blog/2025/20250405-cbam-support-program#지원조건-및-자격">하나루프 X (주)로엔컨설팅과 함께하는 정부지원사업 상세내용 링크</a><br/>
 
             <div className="w-full  p-6">
                 <button className="hover:text-white bg-blue-500 text-white rounded-md p-3">
